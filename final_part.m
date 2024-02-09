877ef3c02a030320cbf3eaaada1bf37c1512cd93
hwtomy <57659738+hwtomy@users.noreply.github.com>
2024-02-09 11:39:20 +0100

    test of nc

--- final_part.m
+++ final_part.m
@@ -26,42 +26,42 @@ xfir=[];
 
 
 %% Fir Wiener filter
-for i =1:K
-    y = z(L*(i-1)+1:L*i);
-    sigY = xcovhat(y,y,L);
-    sigV = xcovhat(v,v,L);
-    sigYX = sigY - sigV;
-    sigYY = covhat(y, L);
-    [xhat, theta] = firw(y,sigYX,sigYY);
-    xfir = [xfir,transpose(xhat)];
-end
-%soundsc(xfir,fs);
-figure;
-plot(periodogram(xfir,hanning(length(xfir)), fs));
+% for i =1:K
+%     y = z(L*(i-1)+1:L*i);
+%     sigY = xcovhat(y,y,L);
+%     sigV = xcovhat(v,v,L);
+%     sigYX = sigY - sigV;
+%     sigYY = covhat(y, L);
+%     [xhat, theta] = firw(y,sigYX,sigYY);
+%     xfir = [xfir,transpose(xhat)];
+% end
+% %soundsc(xfir,fs);
+% figure;
+% plot(periodogram(xfir,hanning(length(xfir)), fs));
 
 
 %% non_casual filter
-Nlen =12; %(6,8,10,  12,13,14)
-center_freq = 300;  
-bandwidth =20;
-normalized_freq = [center_freq - bandwidth/2, center_freq + bandwidth/2] / (fs/2);
-sf = fir1(100, normalized_freq, 'bandpass');
- window = hamming(101);
-sf = sf .* window';
-zf = filter(sf, 1, z);
-[Ahat, sigma2hat] = aryule(zf, Nlen);
-[PhixyNum,PhixyDen,PhiyyNum,PhiyyDen] = spec_add(Ahat, sigma2hat, noise_ar, varnoise);
-[xhatnc, numnc, dennc] = ncw(z, PhixyNum, PhixyDen, PhiyyNum, PhiyyDen);
-%soundsc(xhatnc,fs);
-figure;
-plot(periodogram(xhatnc,hanning(length(xhatnc)), fs));
-legend('Before','After');
+% Nlen =12; %(6,8,10,  12,13,14)
+% center_freq = 300;  
+% bandwidth =20;
+% normalized_freq = [center_freq - bandwidth/2, center_freq + bandwidth/2] / (fs/2);
+% sf = fir1(100, normalized_freq, 'bandpass');
+%  window = hamming(101);
+% sf = sf .* window';
+% zf = filter(sf, 1, z);
+% [Ahat, sigma2hat] = aryule(zf, Nlen);
+% [PhixyNum,PhixyDen,PhiyyNum,PhiyyDen] = spec_add(Ahat, sigma2hat, noise_ar, varnoise);
+% [xhatnc, numnc, dennc] = ncw(z, PhixyNum, PhixyDen, PhiyyNum, PhiyyDen);
+% %soundsc(xhatnc,fs);
+% figure;
+% plot(periodogram(xhatnc,hanning(length(xhatnc)), fs));
+% legend('Before','After');
 
 
 
 %% casual wiener filter
 Nlen =12; %(6,8,10,  12,13,14)
-center_freq = 300;  %(2238-2272)
+center_freq = 2310;  %(2238-2272)
 bandwidth =20; %find by 5Hz
 normalized_freq = [center_freq - bandwidth/2, center_freq + bandwidth/2] / (fs/2);
 sf = fir1(100, normalized_freq, 'bandpass');
